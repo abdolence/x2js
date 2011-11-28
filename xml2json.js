ffbadc24c4fb5512734ce15b3e9157a2a603e0a7
Abdulla Abdurakhmanov <abdulla.abdurakhmanov@gmail.com>
2011-11-29 02:23:59 +0400

    Initial release

---
+++ xml2json.js
@@ -0,0 +1,163 @@
+/*
+ Copyright 2011 Abdulla Abdurakhmanov
+ Original sources are available at https://code.google.com/p/x2js/
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+function X2JS() {
+	function parseDOMChildren( node ) {
+	
+		var DOMNodeTypes = {
+			ELEMENT_NODE : 1,
+			TEXT_NODE    : 3,
+			DOCUMENT_NODE : 9
+		}
+	
+		if(node.nodeType == DOMNodeTypes.DOCUMENT_NODE) {
+			var result = new Object;
+			result[node.firstChild.nodeName] = parseDOMChildren(node.firstChild);
+			return result;
+		}
+		else
+		if(node.nodeType == DOMNodeTypes.ELEMENT_NODE) {
+			var result = new Object;
+			
+			var nodeChildren = node.childNodes;
+			
+			for(var cidx=0; cidx <nodeChildren.length; cidx++) {
+				var child = nodeChildren[cidx];
+				if(result[child.nodeName] == null) {
+					if(child.nodeType != DOMNodeTypes.TEXT_NODE)
+						result[child.nodeName] = parseDOMChildren(child);
+					else
+						result = child.nodeValue;
+				}
+				else {
+					if(result[child.nodeName] != null) {
+						if( !(result[child.nodeName] instanceof Array)) {
+							var tmpObj = result[child.nodeName];
+							result[child.nodeName] = new Array(nodeChildren.length);
+							result[child.nodeName][0] = tmpObj;
+						}
+					}
+					var aridx = 0;
+					while(result[child.nodeName][aridx]!=null) aridx++;			
+					(result[child.nodeName])[aridx] = parseDOMChildren(child);
+				}			
+			}
+			
+			return result;
+		}
+		else
+		if(node.nodeType == DOMNodeTypes.TEXT_NODE) {		
+			return node.nodeValue;
+		}	
+	}
+	
+	function startTag(elementName, tns, tagged) {
+		if(tagged) {
+			if(tns == null) {
+				return "<"+elementName+">";
+			}
+			else {
+				return "<"+elementName+" tns=\""+tns+"\">";
+			}				
+		}
+		else
+			return "";
+	}
+	
+	function endTag(elementName, tagged) {
+		if(tagged)
+			return "</"+elementName+">"
+		else
+			return "";
+	}
+	
+	function parseJSONObject ( jsonObj, tagged ) {
+		var result = "";	
+		var jsonObjTNS = null;
+		for( var it in jsonObj  ) {
+			if(it=="_tns")
+				continue;
+			var subObj = jsonObj[it];
+			if(subObj!=null && subObj instanceof Object) {
+				if(subObj instanceof Array) {
+					for(var arIdx = 0; arIdx < subObj.length; arIdx++) {
+						result+=startTag(it, jsonObjTNS, tagged);
+						arrayTagging = subObj[arIdx] instanceof Object;
+						result+=parseJSONObject(subObj[arIdx], arrayTagging);
+						result+=endTag(it, tagged);
+					}
+				}
+				else {
+					jsonObjTNS = subObj["_tns"];				
+					result+=startTag(it, jsonObjTNS, tagged);
+					result+=parseJSONObject(subObj, true);
+					result+=endTag(it, tagged);
+					jsonObjTNS = null;
+				}
+			}
+			else
+			if(subObj instanceof String || typeof (subObj) == "string") {
+				result+=startTag(it, jsonObjTNS, tagged);			
+				result+=subObj;
+				result+=endTag(it, tagged);			
+			}
+		}
+		return result;
+	}
+
+	this.xml2json = function (xmlDoc) {
+		return parseDOMChildren ( xmlDoc );
+	}
+	
+	this.xml_str2json = function (xmlDocStr) {
+		var parser=new DOMParser();
+		xmlDoc=parser.parseFromString(xmlText,"text/xml");
+		return this.xml2json(xmlDoc);
+	}
+
+	this.json2xml_str = function (jsonObj) {
+		return parseJSONObject ( jsonObj, true );	
+	}
+
+	this.json2xml = function (jsonObj) {
+		var parser=new DOMParser();
+		var xmlStr = this.json2xml_str (jsonObj);
+		return parser.parseFromString( xmlStr, "text/xml" );
+	}
+
+	this.testSamples = function() {
+		var xmlText = "<MyOperation><test>value</test><test2><item>ddsfg</item><item>dsdgfdgfd</item></test2></MyOperation>"
+		var parser=new DOMParser();
+		xmlDoc=parser.parseFromString(xmlText,"text/xml");
+			
+		var resultObj = this.xml2json(xmlDoc)
+		var xmlDoc2 = this.json2xml(resultObj)
+		var xmlDocStr = this.json2xml_str(resultObj)
+		alert(resultObj)
+		
+		var testObjNS = {
+			MyOperation : {
+				test: 'value'
+			}
+		}
+		
+		testObjNS.MyOperation._tns = "http://www.example.org/specs/test";
+		
+		alert(this.json2xml_str(testObjNS))
+		
+	}
+}
